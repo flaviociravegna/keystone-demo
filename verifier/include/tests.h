b90ea92355f5a8c9c24af2824e9ef37905cfef72
flaviociravegna <s303398@studenti.polito.it>
2023-09-29 13:30:51 +0200

    set the tests to 0

--- verifier/include/tests.h
+++ verifier/include/tests.h
@@ -2,7 +2,7 @@
 /*************** change the value to enable the related test ***************/
 
 #define RUNTIME_ATTESTATION_PERF_TEST 0
-#define RUNTIME_ATTESTATION_FUNC_TEST_CERT_FAIL_PARSING 1
+#define RUNTIME_ATTESTATION_FUNC_TEST_CERT_FAIL_PARSING 0
 #define RUNTIME_ATTESTATION_FUNC_TEST_CERT_FAIL_VERIF   0
 
 /***************************************************************************/
